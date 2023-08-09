285448d54b4c1e25d25a794d978503b1fed8088a
Luciano Batista <lucianobatista0897@gmail.com>
2023-08-09 05:34:07 -0300

    foi feito um site de pizza completo

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el)=>document.querySelector(el);
 const cl = (el)=>document.querySelectorAll(el);
@@ -20,6 +22,7 @@ pizzaJson.map((item, index)=>{
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
        
 
         c('.pizzaBig img').src = pizzaJson[key].img;
@@ -81,3 +84,110 @@ cl('.pizzaInfo--size').forEach((size, sizeIndex)=>{
     size.classList.add('selected');
   });
 });
+
+//Ação do botão de comprar
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+     //Qual a Pizza?
+//console.log( "pizza"+modalKey);
+     // Qual o tamanho?
+let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+
+let identifier = pizzaJson[modalKey].id+'@'+size;
+
+//identificador o que vai se colocado no carrinho
+let key = cart.findIndex((item)=>item.identifier == identifier);
+
+if(key > -1){
+  cart[key].qt += modalQt
+}else{ 
+
+cart.push({
+  identifier,
+  id:pizzaJson[modalKey].id,
+  size, 
+  qt:modalQt
+})
+}
+updateCart();
+closeModal();
+//console.log('tamanho'+size);
+     //Quantas Pizzas?
+    // console.log('quantidade'+modalQt);
+});
+
+//atualizar o carrinho
+c('.menu-openner').addEventListener('click', () =>{
+  if(cart.length > 0){
+    c('aside').style.left = '0';
+  }
+});
+c('.menu-closer').addEventListener('click', ()=>{
+  c('aside').style.left = '100vw';
+});
+function updateCart(){
+  c('.menu-openner span').innerHTML = cart.length;
+
+  if(cart.length > 0){
+    c('aside').classList.add('show');
+    c('.cart').innerHTML ='';
+
+    let subtotal = 0;
+    let desconto = 0;
+    let total = 0;
+
+    for(let i in cart){
+      let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+      subtotal += pizzaItem.price * cart[i].qt;
+
+      let cartItem = c('.models .cart--item').cloneNode(true);
+
+      let pizzaSizeName ;
+      switch(cart[i].size){
+        case 0:
+          pizzaSizeName = 'P';
+          break;
+        case 1:
+          pizzaSizeName = 'M';
+          break;
+        case 2:
+          pizzaSizeName = 'G';
+          break;
+
+      }
+      
+
+      let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+      cartItem.querySelector('img').src = pizzaItem.img;
+      cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+      cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+      //essa função e para dimuir e almenta a quantidade de pizza 
+      cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+        if(cart[i].qt > 1){
+          cart[i].qt--;
+        }else{
+          cart.splice(i, 1);
+        }
+        updateCart();
+      });
+      cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+        cart[i].qt++;
+        updateCart();
+      });
+
+      c('.cart').append(cartItem);
+      
+    }
+    desconto = subtotal * 0.1;
+    total = subtotal - desconto;
+
+    c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+    c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+    c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
+
+  }else{
+    c('aside').classList.remove('show');
+    c('aside').style.left = '100vw';
+  }
+}
